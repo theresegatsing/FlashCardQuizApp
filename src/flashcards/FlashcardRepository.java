24ff885d20695ffe7b08e17eb99ea9f926a6dc93
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-29 00:44:25 -0500

    added authorship

--- src/flashcards/FlashcardRepository.java
+++ src/flashcards/FlashcardRepository.java
@@ -8,6 +8,14 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Handles loading and saving flashcards from/to a text file.
+ * Each line is stored as: question|answer
+ * 
+ * @author Therese Elvira Mombou Gatsing
+ */
+
 public class FlashcardRepository {
 
 }
