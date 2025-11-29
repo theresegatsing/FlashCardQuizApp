75e72227ca9ccf5fce97022d1526127c9eda841f
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-29 00:40:42 -0500

    added constructor

--- src/flashcards/Flashcard.java
+++ src/flashcards/Flashcard.java
@@ -6,5 +6,13 @@ package flashcards;
  * @author Therese Elvira Mombou Gatsing
  */
 public class Flashcard {
+	
+	private final String question;
+    private final String answer;
+
+    public Flashcard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
 
 }
