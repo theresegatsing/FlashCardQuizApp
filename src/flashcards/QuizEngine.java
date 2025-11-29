08d47c4ec3b22b901034ddc6bf424aeb9f4c4db9
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-29 00:53:29 -0500

    added authorship

--- src/flashcards/QuizEngine.java
+++ src/flashcards/QuizEngine.java
@@ -5,6 +5,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Runs a quiz session on a list of flashcards.
+ * 
+ * @author Therese Elvira Mombou Gatsing
+ */
 public class QuizEngine {
 
 }
