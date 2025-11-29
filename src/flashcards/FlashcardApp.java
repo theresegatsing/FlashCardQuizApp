7dd7a69d520a1ac93a6fef1cd17643d3abc2ed9a
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-29 01:01:48 -0500

    added main method

--- src/flashcards/FlashcardApp.java
+++ src/flashcards/FlashcardApp.java
@@ -16,5 +16,43 @@ import java.util.Scanner;
  * @author Therese Elvira Mombou Gatsing
  */
 public class FlashcardApp {
+	
+	private static final String DATA_FILE = "flashcards.txt";
+
+    public static void main(String[] args) {
+        FlashcardRepository repository = new FlashcardRepository(DATA_FILE);
+        List<Flashcard> deck = new ArrayList<>(repository.load());
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            boolean running = true;
+
+            while (running) {
+                printMenu(deck.size());
+                System.out.print("Choose an option: ");
+                String choice = scanner.nextLine().trim();
+
+                switch (choice) {
+                    case "1":
+                        addFlashcard(scanner, deck);
+                        break;
+                    case "2":
+                        listFlashcards(deck);
+                        break;
+                    case "3":
+                        QuizEngine quiz = new QuizEngine(deck);
+                        quiz.runQuiz(scanner);
+                        break;
+                    case "4":
+                        repository.save(deck);
+                        System.out.println("Flashcards saved. Goodbye!");
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option. Try again.");
+                }
+            }
+        }
+    }
+
 
 }
